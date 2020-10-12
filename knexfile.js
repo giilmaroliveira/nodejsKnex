d58d8028766fa5940d72ba2c04c0758cb60f58be
Gilmar Oliveira <giilmaroliveira@outlook.com>
2020-10-12 11:59:18 -0300

    Configuring and using knex

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'admin',
+      database: 'knex_test'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  }
+};

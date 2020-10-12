5f2497970af1a0e3162f449dbcfe83af02a68288
Gilmar Oliveira <giilmaroliveira@outlook.com>
2020-10-12 13:57:54 -0300

    Adding projects operations and pagination

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const routes = express.Router();
-const userController = require('./controllers/userController');
 
-routes.get('/api/users', userController.index);
-routes.post('/api/users', userController.create);
-routes.put('/api/users/:id', userController.update);
-routes.delete('/api/users/:id', userController.delete);
+const userController = require('./controllers/userController');
+const projectController = require('./controllers/projectController');
+
+routes
+  // Users
+  .get('/api/users', userController.index)
+  .post('/api/users', userController.create)
+  .put('/api/users/:id', userController.update)
+  .delete('/api/users/:id', userController.delete)
+  // Projects
+  .get('/api/projects', projectController.index)
+  .post('/api/projects', projectController.create);
 
 module.exports = routes;

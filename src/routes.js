b2c6cce5bc5325f71907a0462624b86fd9e2abdd
Gilmar Oliveira <giilmaroliveira@outlook.com>
2020-10-12 12:16:23 -0300

    adding post, put and delete operations for users

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const routes = express.Router();
 const userController = require('./controllers/userController');
 
-routes.get('/users', userController.index);
+routes.get('/api/users', userController.index);
+routes.post('/api/users', userController.create);
+routes.put('/api/users/:id', userController.update);
+routes.delete('/api/users/:id', userController.delete);
 
 module.exports = routes;
